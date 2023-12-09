7a313904bd4567f9ead106558cd4b819aaa7b673
Aastha Pinhatiya <aasthapinhatia@gmail.com>
2023-12-09 23:30:16 +0530

    update in root.page

--- lib/views/root_page.dart
+++ lib/views/root_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RootPage extends StatelessWidget {
-  const RootPage({super.key});
+  const RootPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,100 +14,137 @@ class RootPage extends StatelessWidget {
           vertical: 24,
           horizontal: 16,
         ),
-        child: Column(
-          children: [
-            //HEADER
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hello, Shanaya',
-                      style: TextStyle(
-                        color: Color(0xFF111111),
-                        fontSize: 24,
-                        fontFamily: 'Manrope',
-                        fontWeight: FontWeight.w700,
-                        height: 0,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, // Align text to the left
+            children: [
+              //HEADER
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hello, Shanaya',
+                        style: TextStyle(
+                          color: Color(0xFF111111),
+                          fontSize: 24,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w700,
+                          height: 0,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Find the service you want, and treat yourself',
+                        style: TextStyle(
+                          color: Color(0xFF50555C),
+                          fontSize: 14,
+                          fontFamily: 'Nunito Sans',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                          letterSpacing: 0.24,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: ShapeDecoration(
+                      color: Colors.amber,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
                       ),
                     ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Find the service you want, and treat yourself',
-                      style: TextStyle(
-                        color: Color(0xFF50555C),
-                        fontSize: 14,
-                        fontFamily: 'Nunito Sans',
-                        fontWeight: FontWeight.w400,
-                        height: 0,
-                        letterSpacing: 0.24,
-                      ),
-                    ),
-                  ],
-                ),
-                // const SizedBox(width: 24),
-                Container(
-                  decoration: ShapeDecoration(
-                    color: Colors.amber,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
+                    child: IconButton(
+                      color: Colors.white,
+                      icon: const Icon(Icons.search),
+                      onPressed: () {},
                     ),
                   ),
-                  child: IconButton(
-                    color: Colors.white,
-                    icon: const Icon(Icons.search),
-                    onPressed: () {},
+                ],
+              ),
+              const SizedBox(height: 16),
+              //PROMO
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            //PROMO
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: 300,
-                      height: 118,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        color: Colors.amber,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 118,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Colors.amber,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 8),
-                    Container(
-                      width: 300,
-                      height: 118,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        color: Colors.amber,
+                      const SizedBox(width: 8),
+                      Container(
+                        width: 300,
+                        height: 118,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Colors.amber,
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 8),
-                    Container(
-                      width: 300,
-                      height: 118,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24),
-                        color: Colors.amber,
+                      const SizedBox(width: 8),
+                      Container(
+                        width: 300,
+                        height: 118,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(24),
+                          color: Colors.amber,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              // Added Text widget aligned to the left
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'This is the end of the content',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Added GridView
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  // return GestureDetector(
+                  //   onTap: () {
+                  //     print('Tapped on item $index');
+
+                  //   },
+                  //   child: Image.asset(
+                  //     'assets/image_$index.png', // Replace with your image paths
+                  //     fit: BoxFit.cover,
+                  //   ),
+                  // );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
