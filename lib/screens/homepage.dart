2eba18c384c83e77b0f74d25a3fcff812e3bad68
Aastha Pinhatiya <aasthapinhatia@gmail.com>
2024-01-01 21:33:43 +0530

    Update homepage.dart

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -495,16 +495,22 @@ class RootPage extends StatelessWidget {
                           horizontal: 16,
                         ),
                         child: Row(
-                          children: List.generate(
-                            3,
-                            (index) {
-                              return const Row(
-                                children: <Widget>[
-                                  //START HERE
-                                ],
-                              );
-                            },
-                          ),
+                          children: [
+                            _buildInterestContainer(
+                              imagePath: "assets/categories/haircut.png",
+                              text: "Haircut",
+                            ),
+                            const SizedBox(width:5),
+                            _buildInterestContainer(
+                              imagePath: "assets/categories/nails.png",
+                              text: "Nails",
+                            ),
+                            const SizedBox(width : 5),
+                            _buildInterestContainer(
+                              imagePath: "assets/categories/facial.png",
+                              text: "Facial",
+                            ),
+                          ]
                         ),
                       ),
                     ),
@@ -623,6 +629,274 @@ class RootPage extends StatelessWidget {
               label: "Profile",
             ),
           ],
+          onTap: (int index){
+            if(index == 1 ){
+              _showNearbyBottomSheet(context);
+            }
+          },
         ));
+  }
+  Widget _buildInterestContainer({required String imagePath, required String text}) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      width: 146,
+      height: 56,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(225, 245, 250, 1),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Center align content
+        children: [
+          Image.asset(
+            imagePath,
+            width: 32,
+            height: 32,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: TextStyle(
+              color: const Color.fromRGBO(255, 214, 0, 1),
+              fontSize: 14,
+              fontFamily: 'Manrope',
+              fontWeight: FontWeight.w600,
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _buildnearbyyellowContainer({required String text}) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      width: 88,
+      height: 32,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(255, 214, 0, 1),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Center align content
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              color: const Color.fromRGBO(255, 255, 255, 1),
+              fontSize: 12,
+              fontFamily: 'Manrope',
+              fontWeight: FontWeight.w600,
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+    Widget _buildnearbywhiteContainer({required String text}) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      width: 88,
+      height: 32,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(255, 255, 255, 1),
+        borderRadius: BorderRadius.circular(100),
+        border: Border.all(
+          color: const Color.fromRGBO(255, 220, 40, 1),
+          width: 2,
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Center align content
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+              color: const Color.fromRGBO(255, 240, 40, 1),
+              fontSize: 12,
+              fontFamily: 'Manrope',
+              fontWeight: FontWeight.w600,
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  void _showNearbyBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          // Your bottom sheet content goes here
+          padding: EdgeInsets.only(top: 20),
+          
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left:16.0),
+                child: Text(
+                  "Nearby Salon List",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Manrope',
+                    
+
+                  ),
+                ),
+              ),
+
+              SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                        ),
+                        child: Row(
+                          children: [
+                            _buildnearbyyellowContainer(
+                              text: "Haircut",
+                            ),
+                            const SizedBox(width:2),
+                            _buildnearbywhiteContainer(
+                              
+                              text: "Nails",
+                            ),
+                            const SizedBox(width : 2),
+                            _buildnearbyyellowContainer(
+                              
+                              text: "Facial",
+                            ),
+                          ]
+                        ),
+                      ),
+                    ),
+                  GestureDetector(
+                    onTap:(){
+                      Navigator.pop(context);
+                    },
+                    child: Padding(
+                      padding:const EdgeInsets.symmetric(horizontal: 16),
+                      child: Container(
+                        height:131,
+                        width: 343,
+                        
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 1,
+                              blurRadius: 3,
+                              offset: Offset(0, 2),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              "assets/salons/Image1.png",
+                              height: 131,
+                              width:125,),
+                              
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15,top: 2),
+                                    child: Text("Hair,Facial",
+                                    style: TextStyle(
+                                      fontSize:12,
+                                      fontFamily: "Nunito Sans",
+                                      fontWeight: FontWeight.w400,
+                                      color: const Color.fromRGBO(255, 240, 40, 1),
+                                    ),),
+                                  ),
+                                 
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15,top: 2),
+                                    child: Text("Looks Salon",
+                                    style: TextStyle(
+                                      fontSize:16,
+                                      fontFamily: "Manrope",
+                                      fontWeight: FontWeight.w700,
+                                      color: const Color.fromRGBO(0, 0, 0, 1),
+                                    ),),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15,top: 2),
+                                    child: Text("CP,Near Khadi India,Cann.",
+                                    style: TextStyle(
+                                      fontSize:14,
+                                      fontFamily: "Nunito Sans",
+                                      fontWeight: FontWeight.w400,
+                                      color: const Color.fromRGBO(0, 0, 0, 1),
+                                    ),),
+                                  ),
+                                  const SizedBox(height: 25),
+                                  const SizedBox(height: 10),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 10.0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                    
+                                        Image.asset("assets/misc/Star.png",
+                                        height: 12.67, width: 13.64,),
+                                        Text("4.7",
+                                          style: TextStyle(
+                                            fontSize:12,
+                                            fontFamily: "Manrope",
+                                            fontWeight: FontWeight.w700,
+                                            color: const Color.fromRGBO(0, 0, 0, 1),
+                                          ),
+                                        ),
+                                        Text("(2.7k)",
+                                          style: TextStyle(
+                                            fontSize:12,
+                                            fontFamily: "Manrope",
+                                            fontWeight: FontWeight.w400,
+                                            color: const Color.fromRGBO(0, 0, 0, 1),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Image.asset("assets/misc/Heart.png",height:24,width: 24,)
+
+                          ],
+                        ),
+
+                      ), 
+                    ),
+                  )
+              // ListTile(
+              //   title: Text('Option 1'),
+              //   onTap: () {
+              //     // Perform action for Option 1
+              //     Navigator.pop(context);
+              //   },
+              // ),
+              // ListTile(
+              //   title: Text('Option 2'),
+              //   onTap: () {
+              //     // Perform action for Option 2
+              //     Navigator.pop(context);
+              //   },
+              // ),
+              // Add more list tiles or widgets as needed
+            ],
+          ),
+        );
+      },
+    );
   }
 }
