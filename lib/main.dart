173953ebb0ada469d2cdb0bfefc15dff1395d74e
Saksham <saksham252003@gmail.com>
2024-01-05 22:23:10 +0530

    shop_details assets

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       //home: const RootPage(),
-      home: BoardingScreen()
+      home: RootPage()
     )));
   }
 }
