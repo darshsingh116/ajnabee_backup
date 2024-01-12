ce6db632408f1ec1d413b5bab0db146d12c4becc
Aastha Pinhatiya <aasthapinhatia@gmail.com>
2024-01-12 21:26:13 +0530

    Update salon_details.dart

--- lib/screens/salon_details.dart
+++ lib/screens/salon_details.dart
@@ -25,8 +25,8 @@ class SalonDetails extends StatelessWidget {
                 child: Image.asset('assets/shop_details/shop.png'),
               ),
               Positioned(
-                left: 10,
-                top: 8,
+                left: 20,
+                top: 25,
                 child: Container(
                   width: 48,
                   height: 48,
@@ -44,7 +44,7 @@ class SalonDetails extends StatelessWidget {
               ),
               Positioned(
                 left: 280,
-                top: 8,
+                top: 25,
                 child: Container(
                   width: 48,
                   height: 48,
@@ -128,17 +128,263 @@ class SalonDetails extends StatelessWidget {
                         ),
                       ],
                     ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: const Color.fromRGBO(249, 134, 0, 1),
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          '4.7',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Nunito Sans',
+                          ),
+                          
+                        ),
+                        const SizedBox(width: 2,),
+                        Text(
+                          '(2.7k)',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Nunito Sans',
+                          ),
+                          
+                        ),
+                        const SizedBox(width:55,),
+                        Icon(
+                          Icons.visibility,
+                          color: const Color.fromRGBO(93, 100, 110, 1),
+                        ),
+                        SizedBox(width: 5),
+                        
+                        Text(
+                          '10k views',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Nunito Sans',
+                            
+                          ),
+                        ),
+                      ],
+                    ),
                     Divider(
                       
                       thickness: 10, 
                       color: Colors.grey, 
-                    )
+                    ),
+                    const SizedBox(height: 20,),
+                    Text(
+                      'About',
+                      style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Manrope',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                     
+                    const SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        Text(
+                          "Keeping up to speed with the market's latest trends \n Plush Beauty Lounge recognizes the need for constant \nimprovements. Our team receives regular training from \n hairdressers all...",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Nunito Sans',
+                            
+                          ),
+                          
+                        ),
+                        
+                        Text(
+                          'Read more',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Nunito Sans'
+
+                            
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20,),
+                    Text(
+                      'Opening Hours',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Manrope',
+                            
+                      ),
+                    ),
+                    const SizedBox(height: 15,),
+                    Row(
+                      children: [
+                        Container(
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(255, 214, 0, 1),
+                          ),
+                        ),
+                        const SizedBox(width: 10,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Monday - Friday',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Nunito Sans',
+                            
+                              ),
+                            ),
+                            Text(
+                              '08:00am - 03:00pm',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                fontFamily: 'Manrope',
+                            
+                              ),
+                            ),
+                          ],
+
+                        ),
+                        const SizedBox(width: 30,),
+                        Container(
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color.fromRGBO(255, 214, 0, 1),
+                          ),
+                        ),
+                        const SizedBox(width: 10,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Saturday - Sunday',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Nunito Sans',
+                            
+                              ),
+                            ),
+                            Text(
+                              '09:00am - 02:00pm',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                fontFamily: 'Manrope',
+                            
+                              ),
+                            ),
+                            const SizedBox(height: 20,),
+
+                          ],
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20,),
+                    
+                    Text(
+                      'Our Services',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'Manrope',
+                            
+                      ),
+                    ),
+                    const SizedBox(height: 14,),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 5,
+                        ),
+                        child: Row(
+                          children: [
+                             _buildInterestContainer(
+                              imagePath: "assets/categories/haircut.png",
+                              text: "Haircut",
+                            ),
+                            const SizedBox(width:5),
+                            _buildInterestContainer(
+                              imagePath: "assets/categories/nails.png",
+                              text: "Nails",
+                            ),
+                            const SizedBox(width : 5),
+                            _buildInterestContainer(
+                              imagePath: "assets/categories/facial.png",
+                              text: "Facial",
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Stack(
+                      children: [
+                        Image.asset(
+                          "assets/shop_details/services/medium_length_cut.png",
+                          height: 114,
+                          width: 114,
+                        )
+                      ],
+                    )  
                   ],
                 ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+  Widget _buildInterestContainer({required String imagePath, required String text}) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      width: 146,
+      height: 56,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(225, 255, 255, 1),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center, // Center align content
+        children: [
+          Image.asset(
+            imagePath,
+            width: 32,
+            height: 32,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            text,
+            style: TextStyle(
+              color: const Color.fromRGBO(255, 214, 0, 1),
+              fontSize: 14,
+              fontFamily: 'Manrope',
+              fontWeight: FontWeight.w600,
+              height: 0,
+            ),
+          ),
+        ],
       ),
     );
   }
